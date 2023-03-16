ed22b0317c164cd78111e53e716deeef040c0a95
ayan-dukie-s <ayansaiyad831@gmail.com>
2023-03-16 03:21:57 -0500

    created structure

--- src/main/java/me/ayansaiyad/lilmo.java
+++ src/main/java/me/ayansaiyad/lilmo.java
@@ -1,7 +1,26 @@
 package me.ayansaiyad;
 
-public class lilmo {
-    public static void main(String[] args) {
+import javax.security.auth.login.LoginException;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class lilmo extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+
+        String token = System.getenv("ayan");
+
+        /*JDABuilder builder = JDABuilder.createDefault(token)
+                .setActivity(Activity.playing("with your mom"))
+                .addEventListeners(new lilmo());
+
+        builder.build();*/
+
+        System.out.println(token);
     }
+
 }
