4780f65b175ccfcc5b9ad71461f3310b68425215
ayan-dukie-s <ayansaiyad831@gmail.com>
2023-03-16 20:06:57 -0500

    Made environmental variable for token; bot is now online

--- src/main/java/me/ayansaiyad/lilmo.java
+++ src/main/java/me/ayansaiyad/lilmo.java
@@ -12,15 +12,13 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class lilmo extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
 
-        String token = System.getenv("ayan");
+        String token = System.getenv("lilmo_key");
 
-        /*JDABuilder builder = JDABuilder.createDefault(token)
+        JDABuilder builder = JDABuilder.createDefault(token)
                 .setActivity(Activity.playing("with your mom"))
                 .addEventListeners(new lilmo());
+        builder.build();
 
-        builder.build();*/
-
-        System.out.println(token);
     }
 
 }
