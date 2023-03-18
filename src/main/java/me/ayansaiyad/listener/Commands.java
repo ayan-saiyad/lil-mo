5e7d29e8b71728e5116bfd200269ec0babe6d8f6
ayan-dukie-s <ayansaiyad831@gmail.com>
2023-03-18 18:14:34 -0500

    enhanced switch statement

--- src/main/java/me/ayansaiyad/listener/Commands.java
+++ src/main/java/me/ayansaiyad/listener/Commands.java
@@ -1,8 +1,35 @@
 package me.ayansaiyad.listener;
 
 
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Commands extends ListenerAdapter {
 
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String message = event.getMessage().getContentRaw();
+        String user = event.getAuthor().getAsTag();
+
+        if (message.startsWith("!")) {
+            String[] args = message.split(" ");
+            String command = args[0];
+            String[] commandArgs = new String[args.length - 1];
+
+            switch (command) {
+                case "avatar" -> event.getChannel().sendMessage(event.getAuthor().getAvatarUrl()).queue();
+
+                case "ping" -> event.getChannel().sendMessage("Pong!").queue();
+
+                case "echo" -> {
+                    event.getChannel().sendMessage(String.join(" ", commandArgs)).queue();
+                    System.out.println(String.join(" ", commandArgs));
+                }
+
+                case "help" -> event.getChannel().sendMessage("```!ping\n!echo <text>\n!avatar <user>```").queue();
+
+                default -> event.getChannel().sendMessage("Invalid command. Use !help to see a list of commands.").queue();
+            }
+        }
+    }
 }
